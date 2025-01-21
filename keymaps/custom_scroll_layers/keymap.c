a920229767700ffbdc08809510f38bed52f37fc8
AJGamma <1260148178@qq.com>
2025-01-21 13:45:46 +0800

    single handed layer!

--- keymaps/custom_scroll_layers/keymap.c
+++ keymaps/custom_scroll_layers/keymap.c
@@ -62,12 +62,13 @@
     }
 
 #define _QWERTY      0
-#define _SYM         1
-#define _NAV         2
-#define _NUM         3
-#define _MOUSE       4
-#define _WASD        5
-#define _DOUBLE      6
+#define _WASD        1
+#define _SINGLE      2          // single handed layer, for stuff like 3D modeling
+#define _SYM         3
+#define _NAV         4
+#define _NUM         5
+#define _MOUSE       6
+#define _DOUBLE      7
 
 
 #define LMTL MT(MOD_LCTL, KC_LEFT)
@@ -75,23 +76,59 @@
 #define RMTD MT(MOD_RGUI, KC_DOWN)
 #define RMTU MT(MOD_RCTL, KC_UP)
 
+///////////////////////////////////////////////////////
+///////////////////Double layer macros/////////////////
+////////////Just for better formatting/////////////////
+///////////////////////////////////////////////////////
+#define a_MDA    MT(MOD_LCTL, KC_A)
+#define a_MDS    MT(MOD_LSFT, KC_S)
+#define a_MDD    MT(MOD_LSFT, KC_D)
+#define a_MDF    MT(MOD_LCTL, KC_F)
+#define a_MDJ    MT(MOD_LCTL, KC_J)
+#define a_MDK    MT(MOD_LSFT, KC_K)
+#define a_MDL    MT(MOD_LSFT, KC_L)
+#define a_MDSEMI MT(MOD_LCTL, KC_SCLN)
+
+
 #define CUSTOM_TAP_LAYER_TIMEOUT 500
 #define SCROLL_MOUSE_DELAY      30
 
 #define DELETE_TAPPING_TERM 100
 
+
+
+const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
+  {{6, 5}, {5, 5}, {4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5}},
+  {{6, 6}, {5, 6}, {4, 6}, {3, 6}, {2, 6}, {1, 6}, {0, 6}},
+  {{6, 7}, {5, 7}, {4, 7}, {3, 7}, {2, 7}, {1, 7}, {0, 7}},
+  {{6, 8}, {5, 8}, {4, 8}, {3, 8}, {2, 8}, {1, 8}, {0, 8}},
+  {{6, 9}, {5, 9}, {4, 9}, {3, 9}, {2, 9}, {1, 9}, {0, 9}},
+  {{6, 0}, {5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}},
+  {{6, 1}, {5, 1}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}},
+  {{6, 2}, {5, 2}, {4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}},
+  {{6, 3}, {5, 3}, {4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3}},
+  {{6, 4}, {5, 4}, {4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}},
+};
+
+
+
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-     [_QWERTY     ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)            },
-     [_SYM        ] =  { ENCODER_CCW_CW(KC_BSPC, KC_DEL),           ENCODER_CCW_CW(KC_BSPC, KC_DEL) },
-     [_NAV        ] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-     [_NUM        ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(QK_REP, QK_REP)            },
-     [_WASD       ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)            },
-     [_DOUBLE     ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)            },
-      [_MOUSE     ] =  { ENCODER_CCW_CW(MS_LEFT, MS_RGHT), ENCODER_CCW_CW(MS_DOWN, MS_UP)                    },
+     [_QWERTY  ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)            },
+     [_SINGLE  ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)            },
+     [_SYM     ] =  { ENCODER_CCW_CW(KC_BSPC, KC_DEL),           ENCODER_CCW_CW(KC_BSPC, KC_DEL)   },
+     [_NAV     ] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
+     [_NUM     ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(QK_REP, QK_REP)            },
+     [_WASD    ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)            },
+     [_DOUBLE  ] =  { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)            },
+     [_MOUSE   ] =  { ENCODER_CCW_CW(MS_LEFT, MS_RGHT), ENCODER_CCW_CW(MS_DOWN, MS_UP)             },
     //                  Encoder 1                                     Encoder 2
 };
 #endif
+
+// #if defined(SWAP_HANDS_ENABLE) && defined(ENCODER_MAP_ENABLE)
+// const uint8_t PROGMEM encoder_hand_swap_config[NUM_ENCODERS] = { 1, 0 };
+// #endif
 
 bool is_scroll_layer_deactivated = false;
 
@@ -117,7 +154,6 @@ bool is_y_held = false;
 bool is_z_held = false;
 bool is_g_held = false;
 
-
 uint16_t last_time_8_released = 0;
 uint16_t last_time_i_released = 0;
 uint16_t last_time_pgup_released = 0;
@@ -140,11 +176,19 @@ uint16_t is_vim_g_timer = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_70(
-                               KC_ESC, KC_2, KC_3, KC_4,             KC_5,                                                                 KC_6, KC_7,       KC_8,     KC_9,    KC_DEL,
-            KC_HOME, KC_GRAVE, KC_1,   KC_W, KC_E, KC_R,             KC_T,                                                                 KC_Y, KC_U,       KC_I,     KC_O,    KC_0,    KC_MINS,   KC_EQL,
-            KC_END,  KC_TAB,   KC_Q,   KC_S, KC_D, KC_F,             KC_G,                                                                 KC_H, KC_J,       KC_K,     KC_L,    KC_P,    KC_LBRC,   KC_RBRC,
-            KC_LCTL, KC_CAPS,  KC_A,   KC_X, KC_C, LT(_MOUSE, KC_V), KC_B, LT(_NUM, KC_SPC),                             KC_ENTER,  KC_N,  KC_M, KC_COMM,    KC_DOT,   KC_SCLN, KC_QUOT, KC_BSLS,
-                     KC_LSFT,  KC_Z,   LMTL, LMTR, KC_LCTL,          KC_LGUI, KC_LALT,   LT(_NAV, KC_ESC),   LT(_SYM, KC_BSPC),  KC_RSFT,  KC_RCTL, KC_RCTL, RMTD,     RMTU,    KC_SLSH, KC_RSFT
+                               KC_ESC, KC_2, KC_3, KC_4,                          KC_5,                                                      KC_6,                 KC_7,    KC_8,    KC_9,   KC_DEL,
+            KC_HOME, KC_GRAVE, KC_1,   KC_W, KC_E, KC_R,                          KC_T,                                                      KC_Y,                 KC_U,    KC_I,    KC_O,   KC_0,    KC_MINS, KC_EQL,
+            KC_END,  KC_TAB,   KC_Q,   KC_S, KC_D, KC_F,                          KC_G,                                                      KC_H,                 KC_J,    KC_K,    KC_L,   KC_P,    KC_LBRC, KC_RBRC,
+            KC_LCTL, KC_CAPS,  KC_A,   KC_X, KC_C, LT(_MOUSE, KC_V),              KC_B, LT(_NUM, KC_SPC),                          KC_ENTER, KC_N,                 KC_M,    KC_COMM, KC_DOT, KC_SCLN, KC_QUOT, KC_BSLS,
+                     KC_LSFT,  KC_Z,   LMTL, LMTR, KC_LCTL,  MT(MOD_LGUI, KC_SPC), KC_LALT,  LT(_NAV, KC_ESC), LT(_SYM, KC_BSPC),  KC_RSFT,  MT(MOD_RCTL, KC_SPC), KC_RCTL, RMTD,    RMTU,   KC_SLSH, KC_RSFT
+
+    ),
+    [_SINGLE] = LAYOUT_split_70(
+                          DF(_QWERTY),  KC_2,  KC_3,  KC_4,    KC_5,                                                                                      KC_6,     KC_7,  KC_8,    KC_9,    KC_DEL,
+            KC_HOME, KC_GRAVE, KC_1,    KC_W,  KC_E,  KC_R,    KC_T,                                                                                      KC_Y,     KC_U,  KC_I,    KC_O,    KC_0,     KC_MINS, KC_EQL,
+            KC_END,  KC_TAB,   KC_Q,    a_MDS, a_MDD, a_MDF,   KC_G,                                                                                      KC_H,     a_MDJ, a_MDK,   a_MDL,   KC_P,     KC_LBRC, KC_RBRC,
+            KC_LCTL, KC_CAPS,  a_MDA,   KC_X,  KC_C,  KC_V,    KC_B,          KC_ENT,                                                             KC_ENT, KC_N,     KC_M,  KC_COMM, KC_DOT,  a_MDSEMI, KC_QUOT, KC_BSLS,
+                     KC_LSFT,  KC_Z,    LMTL,  LMTR,  KC_LCTL, SH_T(KC_SPC),  MT(MOD_LALT, KC_BSPC), KC_ESC, KC_BSPC, MT(MOD_LSFT, KC_ESC), SH_T(KC_SPC), KC_RCTL,  RMTD,    RMTU,    KC_SLSH,  KC_RSFT
 
     ),
     [_SYM] = LAYOUT_split_70(
@@ -157,9 +201,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT_split_70(
 
                               _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______,                                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_END,  KC_F11,  KC_F12,
+            _______, _______, _______, _______, KC_EQL,  KC_PLUS, _______,                                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_END,  KC_F11,  KC_F12,
             _______, _______, _______, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MPRV, _______, _______,
-            _______, _______, KC_LGUI, _______, _______, _______, _______, _______,                   _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______,
+            _______, _______, KC_LGUI, _______, KC_UNDS, KC_MINS, _______, _______,                   _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______,
                      _______, _______, _______, _______, _______, _______, _______, _______, KC_ESC,  _______, _______, _______, KC_BRID, KC_BRIU, KC_MNXT, _______
     ),
     [_NUM] = LAYOUT_split_70(
@@ -178,11 +222,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     ),
     [_DOUBLE] = LAYOUT_split_70(
-                          KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO,                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-            KC_NO, KC_NO, KC_NO, DF(_WASD), KC_NO, KC_NO, KC_NO,                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-            KC_NO, KC_NO, DF(_QWERTY),      KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-            KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                   KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+                          KC_NO,        KC_NO,       KC_NO, KC_NO, KC_NO,                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO,        DF(_WASD),   KC_NO, KC_NO, KC_NO,                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, DF(_QWERTY),  DF(_SINGLE), KC_NO, KC_NO, KC_NO,                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO,        KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO,               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                   KC_NO, KC_NO,        KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
 
     ),
     // [_MT_QWERTY] = LAYOUT_split_70(
