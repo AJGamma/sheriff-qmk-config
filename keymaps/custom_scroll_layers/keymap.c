79366e4feee56eff2dca47d4079ec9d84c7a0def
AJGamma <1260148178@qq.com>
2025-01-22 01:54:01 +0800

    removed thumb mod-tap

--- keymaps/custom_scroll_layers/keymap.c
+++ keymaps/custom_scroll_layers/keymap.c
@@ -176,11 +176,11 @@ uint16_t is_vim_g_timer = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_70(
-                               KC_ESC, KC_2, KC_3, KC_4,                          KC_5,                                                      KC_6,                 KC_7,    KC_8,    KC_9,   KC_DEL,
-            KC_HOME, KC_GRAVE, KC_1,   KC_W, KC_E, KC_R,                          KC_T,                                                      KC_Y,                 KC_U,    KC_I,    KC_O,   KC_0,    KC_MINS, KC_EQL,
-            KC_END,  KC_TAB,   KC_Q,   KC_S, KC_D, KC_F,                          KC_G,                                                      KC_H,                 KC_J,    KC_K,    KC_L,   KC_P,    KC_LBRC, KC_RBRC,
-            KC_LCTL, KC_CAPS,  KC_A,   KC_X, KC_C, LT(_MOUSE, KC_V),              KC_B, LT(_NUM, KC_SPC),                          KC_ENTER, KC_N,                 KC_M,    KC_COMM, KC_DOT, KC_SCLN, KC_QUOT, KC_BSLS,
-                     KC_LSFT,  KC_Z,   LMTL, LMTR, KC_LCTL,  MT(MOD_LGUI, KC_SPC), KC_LALT,  LT(_NAV, KC_ESC), LT(_SYM, KC_BSPC),  KC_RSFT,  MT(MOD_RCTL, KC_SPC), KC_RCTL, RMTD,    RMTU,   KC_SLSH, KC_RSFT
+                               KC_ESC, KC_2, KC_3, KC_4,              KC_5,                                                                KC_6,  KC_7,    KC_8,    KC_9,   KC_DEL,
+            KC_HOME, KC_GRAVE, KC_1,   KC_W, KC_E, KC_R,              KC_T,                                                                KC_Y,  KC_U,    KC_I,    KC_O,   KC_0,    KC_MINS, KC_EQL,
+            KC_END,  KC_TAB,   KC_Q,   KC_S, KC_D, KC_F,              KC_G,                                                                KC_H,  KC_J,    KC_K,    KC_L,   KC_P,    KC_LBRC, KC_RBRC,
+            KC_LCTL, KC_CAPS,  KC_A,   KC_X, KC_C, LT(_MOUSE, KC_V),  KC_B, LT(_NUM, KC_SPC),                                    KC_ENTER, KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SCLN, KC_QUOT, KC_BSLS,
+                     KC_LSFT,  KC_Z,   LMTL, LMTR, KC_LCTL,           KC_LGUI, KC_LALT,  LT(_NAV, KC_ESC), LT(_SYM, KC_BSPC),  KC_RSFT,  KC_RCTL, KC_RCTL, RMTD,    RMTU,   KC_SLSH, KC_RSFT
 
     ),
     [_SINGLE] = LAYOUT_split_70(
